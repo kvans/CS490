3a12700b5df69ad0a76837ddf9e508d60db23f83
Brian Hernandez <behr328@gmail.com>
2016-07-14 19:18:38 -0400

    Added login.php

---
+++ middle/login.php
@@ -0,0 +1,22 @@
+<?php
+
+#
+# Service allowing authentication of users login given
+# a username and password. Clients of this service can
+# authenticate users using an HTTP POST with the username
+# and password given as part of the request body. 
+#
+# Author: Brian Hernandez
+#
+
+include "backend/query_db.php";
+
+$username = $_POST["username"];
+$password = $_POST["password"];
+
+if (isValidLogin($username, $password)) {
+    echo "{ isValid: true }";
+}
+else {
+    echo "{ isValid: false }";
+}
