36fa66a4a93abad054a4336ef8b4779a65406116
Brian Hernandez <behr328@gmail.com>
2016-07-18 20:12:41 -0400

    Added boilerplate for add_question endpoint

---
+++ middle/add_question.php
@@ -0,0 +1,18 @@
+<?php
+
+#
+# Service for adding questions to the question bank.
+#
+# Author: Brian Hernandez
+#
+
+$question = $_POST["question"];
+
+$input1 = $_POST["input1"];
+$input2 = $_POST["input2"];
+$input3 = $_POST["input3"];
+
+$correct1 = $_POST["correct1"];
+$correct2 = $_POST["correct2"];
+$correct3 = $_POST["correct3"];
+
