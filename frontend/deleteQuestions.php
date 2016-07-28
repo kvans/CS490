9bd9ed88e3828ea0dd5ce6fd26412a216aa9921c
Brian Hernandez <behr328@gmail.com>
2016-07-27 21:49:22 -0400

    Fixed view questions

--- frontend/deleteQuestions.php
+++ frontend/deleteQuestions.php
@@ -1,18 +1,15 @@
 <?php
-        $link = mysqli_connect('sql2.njit.edu', 'kv96', 'PQrAbwHR');
-        if (!$link) {
-                die('Could not connect: ' . mysql_error());
-        }
-        mysqli_select_db($link, 'kv96') or die(mysqli_error());
-        $Question = $_GET['id'];
-        $query = "DELETE FROM CreateOE WHERE Question = '$Question'";
-        $result = mysqli_query($link, $query);
-        if($result){
-                echo "Deleted Successfully";
-                echo "<br>";
-                header("refresh:1; url=viewQuestions.php");
-        }
-        else{
-                echo "ERROR";
-        }
-?>
+    include "../backend/query_db.php";
+    $link = connectToDatabase();
+    global $questionsTable;
+    $Question = $_GET['id'];
+    $query = "DELETE FROM " . $questionsTable . " WHERE Question = '$Question'";
+    $result = mysqli_query($link, $query);
+    if ($result) {
+        echo "Deleted Successfully";
+        echo "<br>";
+        header("refresh:1; url=viewQuestions.php");
+    }
+    else {
+        echo "ERROR";
+    }
--- frontend/viewQuestions.php
+++ frontend/viewQuestions.php
@@ -7,27 +7,27 @@
 <?php
         include "../backend/query_db.php";
         $link = connectToDatabase();
-        $query = "SELECT * FROM CreateOE";
+        global $questionsTable;
+        $query = "SELECT * FROM " . $questionsTable;
         $result = mysqli_query($link, $query);
-        if($result == NULL){
-                echo "Failed";
+        if ($result == NULL) {
+            echo "Failed";
         }
 
         echo "<b><center>Database Output</center></b><br><br>";
 
         while($row = mysqli_fetch_array($result)){
+            $Question = $row['Question'];
+            $Input1 = $row['Input1'];
+            $Input2 = $row['Input2'];
+            $Input3 = $row['Input3'];
+            $Correct1 = $row['Correct1'];
+            $Correct2 = $row['Correct2'];
+            $Correct3 = $row['Correct3'];
 
-                $Question = $row['Question'];
-                $Input1 = $row['Input1'];
-                $Input2 = $row['Input2'];
-                $Input3 = $row['Input3'];
-                $Correct1 = $row['Correct1'];
-                $Correct2 = $row['Correct2'];
-                $Correct3 = $row['Correct3'];
-
-                echo "<fieldset><b>$Question</b><br><br><b>$Input1:</b> $Correct1<br><b>$Input2:</b> $Correct2<br><b>$Input3:</b> $Correct3<br></fieldset>";
-                echo "<a href=\"deleteQuestion.php?id=$Question\">Delete</a>";
-                echo "<br><br>";
+            echo "<fieldset><b>$Question</b><br><br><b>$Input1:</b> $Correct1<br><b>$Input2:</b> $Correct2<br><b>$Input3:</b> $Correct3<br></fieldset>";
+            echo "<a href=\"deleteQuestions.php?id=$Question\">Delete</a>";
+            echo "<br><br>";
         }    
         mysqli_close($link);
 ?>
