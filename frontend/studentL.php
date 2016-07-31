042ca217df8c975b732c2042ec152e8a161d0153
Donnie Ray Damon Jr <Drd8@njit.edu>
2016-07-31 15:44:34 -0400

    Update studentL.php
    Added CSS and dropdown for posting EID

--- frontend/studentL.php
+++ frontend/studentL.php
@@ -3,28 +3,68 @@
 <head>
 <title>Welcome Student</title>
 <style>
-        div.feature {
-                position: relative;
+         ul {
+                list-style-type: none;
+                margin: 0;
+                padding: 0;
+                overflow: hidden;
+                background-color: #333;
         }
-        div.feature a {
+        li {
+                float: left;
+                border-right: 1px solid #bbb;
+        }
+        li:last-child {
+                border-right: none;
+        }
+        li a {
                 display: block;
-                margin: auto;
-                border-style: solid;
-                background-color: lightblue;
-                width: 100px;
-                height: 100%;
+                color: white;
+                text-align: center;
+                padding: 14px 16px;
                 text-decoration: none;
-                z-index: 10;
+        }
+        li a:hover {
+                background-color: #111;
+        }
+         .button {
+                background-color: #4CAF50;
+                border: none;
+                color: white;
+                padding: 15px 32px;
+                text-align: center;
+                text-decoration: none;
+                display: inline-block;
+                font-size: 16px;
+                margin: 4px 2px;
+                cursor: pointer;
         }
 </style>
 </head>
-<h1 align="center">Welcome Student</h1>
 <body>
-        <div class="feature">
-                <a href="takeExam.php">Take Exam</a>
-        </div>
-        <div class="feature">
-                <a href="">View Results</a>
-        </div>
+        <ul>
+                <li><a href="">Examination Nation</a></li>
+                <li><a href="">About</a></li>
+                <li style="float: right"><a href="../middle/logout.php">Logout</a></li>
+        </ul>
+        <h1 align="center">Welcome Student</h1>
+        <?php
+                include "../backend/query_db.php";
+
+                echo "<div class=\"selectExam\">";
+                echo "<center><form action=\"takeExam.php\">";
+                echo "<input list=\"exams\">";
+                echo "<datalist id=\"exams\">";
+                foreach ( DisplayEIDs() as $var ) {
+                        echo "<option value=\"",$var['0'],"\">",$var['1'],"</option>";
+                }
+                echo "</datalist>";
+                echo "<center><button class=\"button\" type=\"submit\">Take Exam</button></center>";
+                echo "</form></center>";
+                echo "</div>";
+        ?>
+        <br>
+        <center><a href="" class="button">View Results</a></center>
 </body>
 </html>
+
