154255fff2830950a4fecfebfb2f3e4ac4ca302d
Brian Hernandez <behr328@gmail.com>
2016-07-31 19:13:20 -0400

    Fixed indentation in instructor landing

--- frontend/instructorL.php
+++ frontend/instructorL.php
@@ -1,61 +1,61 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Welcome Instructor</title>
-<style> 
+    <title>Welcome Instructor</title>
+    <style>
         ul {    
-                list-style-type: none;
-                margin: 0;
-                padding: 0;
-                overflow: hidden; 
-                background-color: #333;
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: #333;
         }
         li {
-                float: left;
-                border-right: 1px solid #bbb;
+            float: left;
+            border-right: 1px solid #bbb;
         }
         li:last-child {
-                border-right: none;
+            border-right: none;
         }
         li a {
-                display: block;
-                color: white;
-                text-align: center;
-                padding: 14px 16px;
-                text-decoration: none;
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
         }
         li a:hover {
-                background-color: #111;
+            background-color: #111;
         }
         .button {
-                background-color: #4CAF50;
-                border: none;
-                color: white;
-                padding: 15px 32px;
-                text-align: center;
-                text-decoration: none;
-                display: inline-block;
-                font-size: 16px;
-                margin: 4px 2px;                
-                cursor: pointer;
+            background-color: #4CAF50;
+            border: none;
+            color: white;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            margin: 4px 2px;
+            cursor: pointer;
         }
-</style>
+    </style>
 </head>
 <body>
-        <ul>
-                <li><a href="">Examination Nation</a></li>
-                <li><a href="">About</a></li>
-                <li style="float: right"><a href="../middle/logout.php">Logout</a></li>
-        </ul>
-        <div class="container">
-        <h1 align="center">Welcome Instructor</h1>
-            <center><a href="addQuestion.html" class="button">Add a Question</a></center>
-        <br>
-            <center><a href="viewQuestions.php" class="button">View Question Bank</a></center>
-        <br>
-            <center><a href="examCreation.php" class="button">Create Exam</a></center>
-        <br>
-            <center><a href="" class="button">View Existing Exams</a></center>
-        </div>
+    <ul>
+        <li><a href="">Examination Nation</a></li>
+        <li><a href="">About</a></li>
+        <li style="float: right"><a href="../middle/logout.php">Logout</a></li>
+    </ul>
+    <div class="container">
+    <h1 align="center">Welcome Instructor</h1>
+        <center><a href="addQuestion.html" class="button">Add a Question</a></center>
+    <br>
+        <center><a href="viewQuestions.php" class="button">View Question Bank</a></center>
+    <br>
+        <center><a href="examCreation.php" class="button">Create Exam</a></center>
+    <br>
+        <center><a href="" class="button">View Existing Exams</a></center>
+    </div>
 </body>
 </html>
