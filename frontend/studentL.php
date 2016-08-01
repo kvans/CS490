0a361a91a529fe568bd3923e03727ea3a9e17ef4
Donnie Ray Damon Jr <Drd8@njit.edu>
2016-08-01 15:54:25 -0400

    Update studentL.php

--- frontend/studentL.php
+++ frontend/studentL.php
@@ -4,55 +4,14 @@
 <html>
 <head>
     <title>Welcome Student</title>
-    <style>
-         ul {
-            list-style-type: none;
-            margin: 0;
-            padding: 0;
-            overflow: hidden;
-            background-color: #333;
-        }
-        li {
-            float: left;
-            border-right: 1px solid #bbb;
-        }
-        li:last-child {
-            border-right: none;
-        }
-        li a {
-            display: block;
-            color: white;
-            text-align: center;
-            padding: 14px 16px;
-            text-decoration: none;
-        }
-        li a:hover {
-            background-color: #111;
-        }
-        .selectExam {
-            text-align: center;
-        }
-        .button {
-            min-width: 300px;
-            background-color: #4CAF50;
-            border: none;
-            color: white;
-            padding: 15px 32px;
-            text-align: center;
-            text-decoration: none;
-            display: inline-block;
-            font-size: 16px;
-            margin: 4px 2px;
-            cursor: pointer;
-        }
-    </style>
+    <link rel="stylesheet" href="boxedStylesheet.css">
 </head>
 <body>
 
     <ul>
-        <li><a href="">Examination Nation</a></li>
+        <li><a href="studentL.php">Examination Nation</a></li>
         <li><a href="">About</a></li>
-        <li style="float: right"><a href="../middle/logout.php">Logout</a></li>
+        <li style="float: right"><a href="../logout.php">Logout</a></li>
     </ul>
 
     <h1 align="center">Welcome Student</h1>
